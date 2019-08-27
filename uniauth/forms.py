91e4fdf6cf407b630e1d0b44ebe2b040e0570d10
peppelinux <giuseppe.demarco@unical.it>
2019-08-27 14:37:25 +0200

    forms localization fixed with ugettext_lazy

--- uniauth/forms.py
+++ uniauth/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
-from django.utils.translation import gettext as _
+from django.utils.translation import ugettext_lazy as _
 
 
 class LoginForm(AuthenticationForm):

637479e73e24888406c4c403638311ce5beb140d
peppelinux <giuseppe.demarco@unical.it>
2019-08-27 13:01:47 +0200

    better it localization

--- uniauth/forms.py
+++ uniauth/forms.py
@@ -7,18 +7,16 @@ class LoginForm(AuthenticationForm):
     # these are inherited
     # username = forms.CharField()
     # password = forms.CharField(widget=forms.PasswordInput())
-    forget_agreement = forms.BooleanField(label=_("Cancella precedente "
-                                                  "consenso ai dati"),
+    forget_agreement = forms.BooleanField(label=_("Delete previous agreement"),
                                           required=False)
-    forget_login = forms.BooleanField(label=_("Non ricordare l'accesso"),
+    forget_login = forms.BooleanField(label=_("Forget access"),
                                       required=False)
 
 class AgreementForm(forms.Form):
-    CHOICES = ((1, _('Do il mio consenso')),
-               (0, _('Nego il mio consenso')))
+    CHOICES = ((1, _('I give my consent')),
+               (0, _('I deny my consent')))
 
     confirm = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect)
-    dont_show_again = forms.BooleanField(label=_("Non presentare questa schermata "
-                                                 "la prossima volta che "
-                                                 "effettuerò l'accesso"),
+    dont_show_again = forms.BooleanField(label=_("Don't show this screen "
+                                                 "on next login."),
                                          required=False)

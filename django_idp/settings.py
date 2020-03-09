e3cfd7203f85a34fcfa811f07c9cf9f691d946d8
Giuseppe <giuseppe.demarco@unical.it>
2020-03-09 14:58:25 +0100

    settings updated

--- django_idp/settings.py
+++ django_idp/settings.py
@@ -14,9 +14,6 @@ import os
 
 from .settingslocal import *
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 

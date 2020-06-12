847590c92baee8cc43c095c21e582bc67c0f7c32
Giuseppe De Marco <giuseppe.demarco@unical.it>
2020-06-12 09:29:49 +0200

    timezone and things in settingslocal

--- django_idp/settings.py
+++ django_idp/settings.py
@@ -160,15 +160,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/2.0/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
-USE_I18N = True
-USE_L10N = True
-USE_TZ = True
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')

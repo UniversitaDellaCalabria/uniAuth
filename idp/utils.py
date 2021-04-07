81b2be1ca4c3adf3526d3158806c9564b128188f
peppelinux <giuseppe.demarco@unical.it>
2021-04-08 01:20:13 +0200

    breakage: moved project to example folder

--- idp/utils.py
+++
@@ -1,6 +0,0 @@
-from django.conf import settings
-
-def get_encoded_username(username):
-    if isinstance(username, list):
-        username = username[0]
-    return '@'.join((username, settings.LDAP_BASE_DOMAIN))
